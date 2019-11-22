a82ab90869cceefe75087917a1d4eaf18b67f2e9
AidanGamzer <52653690+AidanGamzer@users.noreply.github.com>
2019-11-22 20:18:53 +0000

    Fix bugs with certain machines

--- rera1n.sh
+++ rera1n.sh
@@ -1,5 +1,142 @@
 #!/bin/bash
 # Over WiFi or USB
+main_menu() {
+    echo "[*] Which of the following option sets would you like to use: "
+    echo "[*] Recovery options (1)"
+    echo "[*] Tool options - W.I.P (2)"
+    echo "[*] SSH Window (3)"
+    echo "[*] Exit (X)"
+    read -p "[*] Select an option using the shortened option names: " option
+    if [ $option = 1 ] 
+    then
+        recovery_options
+    elif [ $option = 2 ]
+    then
+        tool_options
+    elif [ $option = 3 ]
+    then 
+        ssh_window
+    elif [ $option = C ]
+    then 
+        echo "[*] Developer: a_i_da_n"
+        echo "[*] Usefull components libimobiledevice"
+        echo "[*] Usefull components rcg4u"
+        echo "[*] Original idea ConsoleLogLuke"
+        main_menu
+    elif [ $option = X ]
+    then
+        echo "[-] Exiting program"
+        exit
+    else 
+        echo "[-] Unexpected input exiting program"
+        exit
+    fi
+}
+
+recovery_options() {
+    echo "[*] Which of the following options would you like to use: "
+    echo  "[*] Respring - SpringBoard (1)"
+    echo  "[*] Respring - backboardd (2)"
+    echo  "[*] Reboot (3)"
+    echo  "[*] Respring - Loop Fix (4)"
+    echo "[*] Kill specified process (5)"
+    echo "[*] Restore/Update to signed firmwares (6)"
+    echo "[*] Back to main menu (<)"
+    read -p "[*] Select an option using the shortened option names: " option
+    if [ $option = "1" ] 
+    then
+        sshpass -p$idevicepassword ssh root@$ideviceip -p $ideviceport killall -9 SpringBoard
+        main_menu
+    elif [ $option = "2" ] 
+    then
+        sshpass -p$idevicepassword ssh root@$ideviceip -p $ideviceport killall -9 backboardd
+        main_menu
+    elif [ $option = "3" ] 
+    then
+        sshpass -p$idevicepassword ssh root@$ideviceip -p $ideviceport reboot
+        main_menu
+    elif [ $option = "4" ] 
+    then
+        sshpass -p$idevicepassword ssh root@$ideviceip -p $ideviceport killall -8 SpringBoard
+        main_menu
+    elif [ $option = "5" ]
+    then
+        read -p "[*] Please enter the Proccess name to kill: " processname
+        sshpass -p$idevicepassword ssh root@$ideviceip -p $ideviceport killall -9 $processname
+        main_menu
+    elif [ $option = "6" ]
+    then
+        idevicerestore -l
+        main_menu
+    elif [ $option = "<" ]
+    then
+        main_menu
+    else
+        echo "Unknown or Unrecognised command number"
+    fi
+ }
+
+tool_options() {
+    echo "[*] Which of the following options would you like to use: "
+    echo  "[*] UiCache (1)"
+    echo  "[*] iDevice Info V1.2 (2)"
+    echo "[*] Install Packages - bundle indetifier required (3)"
+    echo "[*] Remove Packages - bundle identifier required (4)"
+    echo "[*] Back to main menu (<)"
+    read -p "[*] Select and option using the shortened option names: " option
+    if [ $option = 3 ]
+    then
+        read -p "[*] Enter the bundle identifier of the package: " idevicebundleidentifier
+        sshpass -p$idevicepassword ssh root@$ideviceip -p $ideviceport apt-get install $idevicebundleidentifier
+        main_menu
+    elif [ $option = 4 ]
+    then 
+        read -p "[*] Enter the bundle identifier of the package: " idevicebundleidentifier
+        sshpass -p$idevicepassword ssh root@$ideviceip -p $ideviceport apt-get remove $idevicebundleidentifier
+        main_menu
+    elif [ $option = "1" ]
+    then
+        sshpass -p$idevicepassword ssh mobile@$ideviceip -p $ideviceport uicache
+        main_menu
+    elif [ $option = "2" ]
+    then
+        ideviceinfo > ideviceinfo.txt
+        grep ActivationState: ideviceinfo.txt
+        grep BasebandVersion: ideviceinfo.txt
+        grep BluetoothAddress: ideviceinfo.txt
+        grep BuildVersion: ideviceinfo.txt
+        grep CPUArchitecture: ideviceinfo.txt
+        grep DeviceClass: ideviceinfo.txt
+        grep DeviceColor: ideviceinfo.txt
+        grep EthernetAddress: ideviceinfo.txt
+        grep FirmwareVersion: ideviceinfo.txt
+        grep HardwareModel: ideviceinfo.txt
+        grep HardwarePlatform: ideviceinfo.txt
+        grep PasswordProtected: ideviceinfo.txt
+        grep ProductType: ideviceinfo.txt
+        grep ProductVersion: ideviceinfo.txt
+        grep -w SerialNumber: ideviceinfo.txt
+        grep WiFiAddress: ideviceinfo.txt
+        main_menu
+    elif [ $option = "5" ]
+    then
+        scp -p -R 2222 root$ideviceip:/* . 
+    elif [ $option = "<" ]
+    then
+        main_menu
+    else
+        echo "[-] Unexpected option selected"
+        exit
+    fi
+}
+
+ssh_window() {
+    echo "[*] Connecting to iDevice"
+    sshpass -p$idevicepassword ssh root@$ideviceip -p $ideviceport 
+    echo "[*] Sucessfully exited SSH window"
+    main_menu
+}
+
 echo [*] Welcome to ReRa1n
 echo "[*] Checking for dependencies" 
 if [ ! -e rerain-dep ]
@@ -12,7 +149,7 @@ then
     echo "[*] Package manager supported"
     mkdir rerain-dep
     cd rerain-dep
-    sudo apt-get install libgcrypt20-doc gnutls-doc gnutls-bin usbmuxd git libplist-dev libplist++ python2.7-dev python3-dev libusbmuxd4 libreadline6-dev make libusb-dev libusb-1.0 openssl libimobiledevice-dev libzip-dev libcurl4-openssl-dev libssl-dev
+    sudo apt-get install libgcrypt20-doc gnutls-doc gnutls-bin usbmuxd git libplist-dev libplist++ python2.7-dev python3-dev libusbmuxd4 libreadline6-dev make libusb-dev openssl libimobiledevice-dev libzip-dev libcurl4-openssl-dev libssl-dev sshpass
     git clone https://github.com/libimobiledevice/libplist
     cd libplist
     ./autogen.sh
@@ -49,6 +186,18 @@ then
     cd ..
     git clone https://github.com/libimobiledevice/idevicerestore
     cd idevicerestore
+    ./autogen.sh
+    make
+    sudo make install
+    cd ..
+    git clone https://github.com/tihmstar/libgeneral
+    cd libgeneral
+    ./autogen.sh
+    make
+    sudo make install
+    cd ..
+    git clone https://github.com/tihmstar/img4tool
+    cd img4tool
     ./autogen.sh
     make
     sudo make install
@@ -138,142 +287,3 @@ else
     echo [-] Unknown or Unavailable selected.
     exit 
 fi
-main_menu() {
-    echo "[*] Which of the following option sets would you like to use: "
-    echo "[*] Recovery options (1)"
-    echo "[*] Tool options - W.I.P (2)"
-    echo "[*] SSH Window (3)"
-    echo "[*] Exit (X)"
-    read -p "[*] Select an option using the shortened option names: " option
-    if [ $option = 1 ] 
-    then
-        recovery_options
-    elif [ $option = 2 ]
-    then
-        tool_options
-    elif [ $option = 3 ]
-    then 
-        ssh_window
-    elif [ $option = C ]
-    then 
-        echo "[*] Developer: a_i_da_n"
-        echo "[*] Usefull components libimobiledevice"
-        echo "[*] Usefull components rcg4u"
-        echo "[*] Original idea ConsoleLogLuke"
-        main_menu
-    elif [ $option = X ]
-    then
-        echo "[-] Exiting program"
-        exit
-    else 
-        echo "[-] Unexpected input exiting program"
-        exit
-    fi
-}
-
-recovery_options() {
-    echo "[*] Which of the following options would you like to use: "
-    echo  "[*] Respring - SpringBoard (1)"
-    echo  "[*] Respring - backboardd (2)"
-    echo  "[*] Reboot (3)"
-    echo  "[*] Respring - Loop Fix (4)"
-    echo "[*] Kill specified process (5)"
-    echo "[*] Restore/Update to signed firmwares (6)"
-    echo "[*] Back to main menu (<)"
-    read -p "[*] Select an option using the shortened option names: " option
-    if [ $option = "1" ] 
-    then
-        sshpass -p$idevicepassword ssh root@$ideviceip -p $ideviceport killall -9 SpringBoard
-        main_menu
-    elif [ $option = "2" ] 
-    then
-        sshpass -p$idevicepassword ssh root@$ideviceip -p $ideviceport killall -9 backboardd
-        main_menu
-    elif [ $option = "3" ] 
-    then
-        sshpass -p$idevicepassword ssh root@$ideviceip -p $ideviceport reboot
-        main_menu
-    elif [ $option = "4" ] 
-    then
-        sshpass -p$idevicepassword ssh root@$ideviceip -p $ideviceport killall -8 SpringBoard
-        main_menu
-    elif [ $option = "5" ]
-    then
-        read -p "[*] Please enter the Proccess name to kill: " processname
-        sshpass -p$idevicepassword ssh root@$ideviceip -p $ideviceport killall -9 $processname
-        main_menu
-    elif [ $option = "6" ]
-    then
-        idevicerestore -l
-        main_menu
-    elif [ $option = "<" ]
-    then
-        main_menu
-    else
-        echo "Unknown or Unrecognised command number"
-    fi
- }
-
-tool_options() {
-    echo "[*] Which of the following options would you like to use: "
-    echo  "[*] UiCache (1)"
-    echo  "[*] iDevice Info (2)"
-    echo "[*] Install Packages - bundle indetifier required (3)"
-    echo "[*] Remove Packages - bundle identifier required (4)"
-    echo "[*] iDevice Info V1.1 - Experimental (5)"
-    echo "[*] Back to main menu (<)"
-    read -p "[*] Select and option using the shortened option names: " option
-    if [ $option = 3 ]
-    then
-        read -p "[*] Enter the bundle identifier of the package: " idevicebundleidentifier
-        sshpass -p$idevicepassword ssh root@$ideviceip -p $ideviceport apt-get install $idevicebundleidentifier
-        main_menu
-    elif [ $option = 4 ]
-    then 
-        read -p "[*] Enter the bundle identifier of the package: " idevicebundleidentifier
-        sshpass -p$idevicepassword ssh root@$ideviceip -p $ideviceport apt-get remove $idevicebundleidentifier
-        main_menu
-    elif [ $option = "1" ]
-    then
-        sshpass -p$idevicepassword ssh mobile@$ideviceip -p $ideviceport uicache
-        main_menu
-    elif [ $option = "2" ]
-    then
-        sshpass -p$idevicepassword ssh root@$ideviceip -p $ideviceport sw_vers
-        main_menu
-    elif [ $option = "<" ]
-    then
-        main_menu
-    elif [ $option = 5 ] 
-    then 
-        ideviceinfo > ideviceinfo.txt
-        grep ActivationState: ideviceinfo.txt
-        grep BasebandVersion: ideviceinfo.txt
-        grep BluetoothAddress: ideviceinfo.txt
-        grep BuildVersion: ideviceinfo.txt
-        grep CPUArchitecture: ideviceinfo.txt
-        grep DeviceClass: ideviceinfo.txt
-        grep DeviceColor: ideviceinfo.txt
-        grep EthernetAddress: ideviceinfo.txt
-        grep FirmwareVersion: ideviceinfo.txt
-        grep HardwareModel: ideviceinfo.txt
-        grep HardwarePlatform: ideviceinfo.txt
-        grep PasswordProtected: ideviceinfo.txt
-        grep ProductType: ideviceinfo.txt
-        grep ProductVersion: ideviceinfo.txt
-        grep -w SerialNumber: ideviceinfo.txt
-        grep WiFiAddress: ideviceinfo.txt
-        main_menu
-    else
-        echo "[-] Unexpected option selected"
-        exit
-    fi
-}
-
-ssh_window() {
-    echo "[*] Connecting to iDevice"
-    sshpass -p$idevicepassword ssh root@$ideviceip -p $ideviceport 
-    echo "[*] Sucessfully exited SSH window"
-    main_menu
-}
-
